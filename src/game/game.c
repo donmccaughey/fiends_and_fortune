4b31764c355f3bf1afbe0305c3e44af3fd1f6634
Don McCaughey <don@ablepear.com>
2016-03-13 17:01:06 -0700

    Display a menu of actions in console game.
    Add actions to an ncurses menu and display it.

--- src/game/game.c
+++ src/game/game.c
@@ -1,8 +1,21 @@
 #include "game.h"
 
 #include <ncurses.h>
+#include <menu.h>
 
 #include "common/alloc_or_die.h"
+
+
+struct {
+    char const *name;
+    char const *description;
+} action_menu_items[] = {
+    { "character", "Create Character" },
+    { "treasure", "Generate Treasure" },
+    { "dungeon", "Generate Dungeon" },
+    { "quit", "Quit" }
+};
+int action_menu_items_count = sizeof action_menu_items / sizeof action_menu_items[0];
 
 
 struct game *
@@ -23,11 +36,41 @@ int
 game_play(struct game *game)
 {
     initscr();
+    cbreak();
+    noecho();
+    keypad(stdscr, TRUE);
+    
     wborder(stdscr, '|', '|', '-', '-', '+', '+', '+', '+');
-    move(1, 1);
-    printw("Hello, world!");
+    
+    ITEM **items = calloc_or_die(action_menu_items_count + 1, sizeof(ITEM *));
+    for (int i = 0; i < action_menu_items_count; ++i) {
+        items[i] = new_item(action_menu_items[i].name, action_menu_items[i].description);
+    }
+    
+    MENU *menu = new_menu(items);
+    mvprintw(LINES - 2, 1, "F1 to Exit");
+    post_menu(menu);
     refresh();
-    getch();
+    
+    int ch;
+    while ((ch = getch()) != KEY_F(1)) {
+        switch (ch) {
+            case KEY_DOWN:
+                menu_driver(menu, REQ_DOWN_ITEM);
+                break;
+            case KEY_UP:
+                menu_driver(menu, REQ_UP_ITEM);
+                break;
+            default:
+                break;
+        }
+    }
+    
+    free_menu(menu);
+    for (int i = 0; i < action_menu_items_count; ++i) {
+        free_item(items[0]);
+    }
+    free_or_die(items);
     endwin();
     return 0;
 }
