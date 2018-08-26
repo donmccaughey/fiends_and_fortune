5f1b4c0f9f9b81efa61ba55b3bfdfcb1f6da4f17
Don McCaughey <don@donm.cc>
2018-08-25 23:54:27 -0700

    Add missing tests in dice_test.c.
    Add tests for functions dice_has_constant_score(), dice_is_valid(),
    dice_make(), dice_make_plus(), dice_make_plus_times(),
    dice_max_base_score(), dice_max_score(), dice_min_base_score() and
    dice_min_score().

--- src/mechanics/dice_test.c
+++ src/mechanics/dice_test.c
@@ -1,4 +1,5 @@
 #include <assert.h>
+#include <limits.h>
 #include <string.h>
 
 #include <base/base.h>
@@ -121,6 +122,97 @@ dice_alloc_range_description_test(void)
     description = dice_alloc_range_description(dice_make_plus_times(1, 4, 1, 10000));
     assert(0 == strcmp("20000-50000", description));
     free_or_die(description);
+}
+
+
+static void
+dice_has_constant_score_test(void)
+{
+    assert( ! dice_has_constant_score(dice_make(2, 6)));
+
+    assert(dice_has_constant_score(dice_make(0, 6)));
+    assert(dice_has_constant_score(dice_make(2, 1)));
+}
+
+
+static void
+dice_is_valid_test(void)
+{
+    assert( ! dice_is_valid(dice_make(-1, 6)));
+    assert( ! dice_is_valid(dice_make(2, 0)));
+    assert( ! dice_is_valid(dice_make(INT_MAX / 6 + 1, 6)));
+
+    assert(dice_is_valid(dice_make(1, 2)));
+    assert(dice_is_valid(dice_make(2, 6)));
+    assert(dice_is_valid(dice_make(INT_MAX / 6, 6)));
+}
+
+
+static void
+dice_make_test(void)
+{
+    struct dice dice = dice_make(2, 6);
+
+    assert(2 == dice.count);
+    assert(6 == dice.sides);
+    assert(0 == dice.modifier);
+    assert(1 == dice.multiplier);
+}
+
+
+static void
+dice_make_plus_test(void)
+{
+    struct dice dice = dice_make_plus(2, 6, 2);
+
+    assert(2 == dice.count);
+    assert(6 == dice.sides);
+    assert(2 == dice.modifier);
+    assert(1 == dice.multiplier);
+}
+
+
+static void
+dice_make_plus_times_test(void)
+{
+    struct dice dice = dice_make_plus_times(2, 6, 2, 10);
+
+    assert(2 == dice.count);
+    assert(6 == dice.sides);
+    assert(2 == dice.modifier);
+    assert(10 == dice.multiplier);
+}
+
+
+static void
+dice_max_base_score_test(void)
+{
+    struct dice dice = dice_make_plus(2, 4, 10);
+    assert(18 == dice_max_base_score(dice));
+}
+
+
+static void
+dice_max_score_test(void)
+{
+    struct dice dice = dice_make_plus_times(2, 4, 10, 100);
+    assert(1800 == dice_max_score(dice));
+}
+
+
+static void
+dice_min_base_score_test(void)
+{
+    struct dice dice = dice_make_plus(2, 4, 10);
+    assert(12 == dice_min_base_score(dice));
+}
+
+
+static void
+dice_min_score_test(void)
+{
+    struct dice dice = dice_make_plus_times(2, 4, 10, 100);
+    assert(1200 == dice_min_score(dice));
 }
 
 
@@ -377,6 +469,15 @@ dice_test(void)
     dice_alloc_base_range_description_test();
     dice_alloc_description_test();
     dice_alloc_range_description_test();
+    dice_has_constant_score_test();
+    dice_is_valid_test();
+    dice_make_test();
+    dice_make_plus_test();
+    dice_make_plus_times_test();
+    dice_max_base_score_test();
+    dice_max_score_test();
+    dice_min_base_score_test();
+    dice_min_score_test();
     dice_parse_test();
     dice_roll_test();
     dice_roll_and_adjust_upwards_test();
