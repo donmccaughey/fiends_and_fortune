7782a25aaeaaa83bdcf6723d9b570684a106d05e
Don McCaughey <don@donm.cc>
2019-08-10 15:56:06 -0700

    Add tests for more dungeon functions.
    New unit tests for
    
        `dungeon_alloc()`
        `dungeon_level_count()`
        `dungeon_starting_level()`
        `dungeon_ending_level()`
        `dungeon_box_for_level()`

--- src/dungeon/dungeon_test.c
+++ src/dungeon/dungeon_test.c
@@ -8,7 +8,21 @@ void
 dungeon_test(void);
 
 
-static void dungeon_generate_small_test(void)
+static void
+dungeon_alloc_test(void)
+{
+    struct dungeon *dungeon = dungeon_alloc();
+
+    assert(dungeon->areas);
+    assert(0 == dungeon->areas_count);
+    assert(dungeon->tiles);
+    assert(0 == dungeon->tiles_count);
+
+    dungeon_free(dungeon);
+}
+
+static void
+dungeon_generate_small_test(void)
 {
     struct dungeon *dungeon = dungeon_alloc();
     
@@ -37,7 +51,131 @@ static void dungeon_generate_small_test(void)
 }
 
 
+static void
+dungeon_level_count_test(void)
+{
+    struct dungeon *dungeon = dungeon_alloc();
+    struct tile *tile;
+
+    assert(0 == dungeon_level_count(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(0, 0, 3));
+    tile->type = tile_type_empty;
+    assert(1 == dungeon_level_count(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(10, 10, 5));
+    tile->type = tile_type_empty;
+    assert(3 == dungeon_level_count(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(-5, -5, 4));
+    tile->type = tile_type_empty;
+    assert(3 == dungeon_level_count(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(5, 5, 2));
+    tile->type = tile_type_empty;
+    assert(4 == dungeon_level_count(dungeon));
+
+    // TODO: test behavior with other tile types
+
+    dungeon_free(dungeon);
+}
+
+
+static void
+dungeon_starting_level_test(void)
+{
+    struct dungeon *dungeon = dungeon_alloc();
+    struct tile *tile;
+
+    assert(0 == dungeon_starting_level(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(0, 0, 3));
+    tile->type = tile_type_empty;
+    assert(3 == dungeon_starting_level(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(-5, -5, 4));
+    tile->type = tile_type_empty;
+    assert(3 == dungeon_starting_level(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(5, 5, 2));
+    tile->type = tile_type_empty;
+    assert(2 == dungeon_starting_level(dungeon));
+
+    // TODO: test behavior with other tile types
+
+    dungeon_free(dungeon);
+}
+
+
+static void
+dungeon_ending_level_test(void)
+{
+    struct dungeon *dungeon = dungeon_alloc();
+    struct tile *tile;
+
+    assert(0 == dungeon_ending_level(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(0, 0, 3));
+    tile->type = tile_type_empty;
+    assert(3 == dungeon_ending_level(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(-5, -5, 4));
+    tile->type = tile_type_empty;
+    assert(4 == dungeon_ending_level(dungeon));
+
+    tile = dungeon_tile_at(dungeon, point_make(5, 5, 7));
+    tile->type = tile_type_empty;
+    assert(7 == dungeon_ending_level(dungeon));
+
+    // TODO: test behavior with other tile types
+
+    dungeon_free(dungeon);
+}
+
+
+static void
+dungeon_box_for_level_test(void)
+{
+    struct dungeon *dungeon = dungeon_alloc();
+    struct tile *tile;
+    struct box box, expected;
+
+    expected = box_make(point_make(0, 0, 2), size_make(0, 0, 1));
+    box = dungeon_box_for_level(dungeon, 2);
+    assert(box_equals(expected, box));
+
+    tile = dungeon_tile_at(dungeon, point_make(5, 5, 2));
+    expected = box_make(point_make(0, 0, 2), size_make(0, 0, 1));
+    box = dungeon_box_for_level(dungeon, 2);
+    assert(box_equals(expected, box));
+
+    tile->type = tile_type_empty;
+    expected = box_make(point_make(5, 5, 2), size_make(1, 1, 1));
+    box = dungeon_box_for_level(dungeon, 2);
+    assert(box_equals(expected, box));
+
+    tile = dungeon_tile_at(dungeon, point_make(10, 5, 2));
+    tile->type = tile_type_empty;
+    expected = box_make(point_make(5, 5, 2), size_make(6, 1, 1));
+    box = dungeon_box_for_level(dungeon, 2);
+    assert(box_equals(expected, box));
+
+    tile = dungeon_tile_at(dungeon, point_make(10, 2, 2));
+    tile->type = tile_type_empty;
+    expected = box_make(point_make(5, 2, 2), size_make(6, 4, 1));
+    box = dungeon_box_for_level(dungeon, 2);
+    assert(box_equals(expected, box));
+
+    dungeon_free(dungeon);
+}
+
+
 void dungeon_test(void)
 {
+    dungeon_alloc_test();
     dungeon_generate_small_test();
+    dungeon_level_count_test();
+    dungeon_starting_level_test();
+    dungeon_ending_level_test();
+    dungeon_box_for_level_test();
 }
