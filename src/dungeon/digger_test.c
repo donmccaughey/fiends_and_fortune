1396372d4f53e19f170c0daba517683e8ecc326c
Don McCaughey <don@donm.cc>
2019-08-11 21:24:46 -0700

    Added unit test for `digger_dig_starting_stairs()` function.

--- src/dungeon/digger_test.c
+++ src/dungeon/digger_test.c
@@ -4,6 +4,7 @@
 #include <base/base.h>
 #include "dungeon.h"
 #include "generator.h"
+#include "tile.h"
 
 
 void
@@ -86,7 +87,6 @@ digger_move_forward_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_move_forward(digger, 3);
@@ -120,7 +120,6 @@ digger_move_backward_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_move_backward(digger, 3);
@@ -154,7 +153,6 @@ digger_move_left_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_move_left(digger, 3);
@@ -188,7 +186,6 @@ digger_move_right_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_move_right(digger, 3);
@@ -222,7 +219,6 @@ digger_ascend_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 4), direction_west);
 
     digger_ascend(digger, 1);
@@ -246,7 +242,6 @@ digger_descend_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_descend(digger, 1);
@@ -270,7 +265,6 @@ digger_spin_180_degrees_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_spin_180_degrees(digger);
@@ -295,7 +289,6 @@ digger_spin_90_degrees_left_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_spin_90_degrees_left(digger);
@@ -320,7 +313,6 @@ digger_spin_90_degrees_right_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     digger_spin_90_degrees_right(digger);
@@ -345,7 +337,6 @@ digger_turn_90_degrees_left_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     // 1  .  .  .
@@ -379,7 +370,6 @@ digger_turn_90_degrees_right_test(void)
     struct dungeon *dungeon = dungeon_alloc();
     struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
     struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
-
     struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
 
     // 1  .  .  b
@@ -407,6 +397,96 @@ digger_turn_90_degrees_right_test(void)
 }
 
 
+static void
+digger_dig_starting_stairs_test(void)
+{
+    struct dungeon *dungeon = dungeon_alloc();
+    struct dungeon_options *dungeon_options = dungeon_options_alloc_default();
+    struct generator *generator = generator_alloc(dungeon, global_rnd, dungeon_options, NULL, NULL);
+    struct digger *digger = digger_alloc(generator, point_make(0, 0, 1), direction_west);
+
+    assert(0 == generator->areas_count);
+    assert(0 == generator->tiles_count);
+
+    digger_dig_starting_stairs(digger);
+
+    assert(1 == generator->areas_count);
+    assert(area_type_stairs_up == generator->areas[0]->type);
+    assert(area_features_none == generator->areas[0]->features);
+    assert(direction_east == generator->areas[0]->direction);
+    struct box expected_box = box_make(point_make(-1, 0, 1), size_make(2, 1, 1));
+    assert(box_equals(expected_box, generator->areas[0]->box));
+
+    // 1  f  f  f  f  .
+    // 0  f  s  s  f  .
+    //-1  .  f  f  f  .
+    //   -2 -1  0  1  2
+    assert(11 == generator->tiles_count);
+
+    assert(point_equals(point_make(-1, -1, 1), generator->tiles[0]->point));
+    assert(tile_type_filled == generator->tiles[0]->type);
+    assert(wall_type_none == generator->tiles[0]->walls.south);
+    assert(wall_type_none == generator->tiles[0]->walls.west);
+
+    assert(point_equals(point_make(0, -1, 1), generator->tiles[1]->point));
+    assert(tile_type_filled == generator->tiles[1]->type);
+    assert(wall_type_none == generator->tiles[1]->walls.south);
+    assert(wall_type_none == generator->tiles[1]->walls.west);
+
+    assert(point_equals(point_make(1, -1, 1), generator->tiles[2]->point));
+    assert(tile_type_filled == generator->tiles[2]->type);
+    assert(wall_type_none == generator->tiles[2]->walls.south);
+    assert(wall_type_none == generator->tiles[2]->walls.west);
+
+
+    assert(point_equals(point_make(-2, 0, 1), generator->tiles[3]->point));
+    assert(tile_type_filled == generator->tiles[3]->type);
+    assert(wall_type_none == generator->tiles[3]->walls.south);
+    assert(wall_type_none == generator->tiles[3]->walls.west);
+
+    assert(point_equals(point_make(-1, 0, 1), generator->tiles[4]->point));
+    assert(tile_type_stairs_up == generator->tiles[4]->type);
+    assert(wall_type_solid == generator->tiles[4]->walls.south);
+    assert(wall_type_none == generator->tiles[4]->walls.west);
+
+    assert(point_equals(point_make(0, 0, 1), generator->tiles[5]->point));
+    assert(tile_type_stairs_up == generator->tiles[5]->type);
+    assert(wall_type_solid == generator->tiles[5]->walls.south);
+    assert(wall_type_none == generator->tiles[5]->walls.west);
+
+    assert(point_equals(point_make(1, 0, 1), generator->tiles[6]->point));
+    assert(tile_type_filled == generator->tiles[6]->type);
+    assert(wall_type_none == generator->tiles[6]->walls.south);
+    assert(wall_type_solid == generator->tiles[6]->walls.west);
+
+
+    assert(point_equals(point_make(-2, 1, 1), generator->tiles[7]->point));
+    assert(tile_type_filled == generator->tiles[7]->type);
+    assert(wall_type_none == generator->tiles[7]->walls.south);
+    assert(wall_type_none == generator->tiles[7]->walls.west);
+
+    assert(point_equals(point_make(-1, 1, 1), generator->tiles[8]->point));
+    assert(tile_type_filled == generator->tiles[8]->type);
+    assert(wall_type_solid == generator->tiles[8]->walls.south);
+    assert(wall_type_none == generator->tiles[8]->walls.west);
+
+    assert(point_equals(point_make(0, 1, 1), generator->tiles[9]->point));
+    assert(tile_type_filled == generator->tiles[9]->type);
+    assert(wall_type_solid == generator->tiles[9]->walls.south);
+    assert(wall_type_none == generator->tiles[9]->walls.west);
+
+    assert(point_equals(point_make(1, 1, 1), generator->tiles[10]->point));
+    assert(tile_type_filled == generator->tiles[10]->type);
+    assert(wall_type_none == generator->tiles[10]->walls.south);
+    assert(wall_type_none == generator->tiles[10]->walls.west);
+
+    digger_free(digger);
+    generator_free(generator);
+    dungeon_options_free(dungeon_options);
+    dungeon_free(dungeon);
+}
+
+
 void
 digger_test(void)
 {
@@ -423,4 +503,5 @@ digger_test(void)
     digger_spin_90_degrees_right_test();
     digger_turn_90_degrees_left_test();
     digger_turn_90_degrees_right_test();
+    digger_dig_starting_stairs_test();
 }
