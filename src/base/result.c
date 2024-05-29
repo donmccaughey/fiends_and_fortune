dc114e18861648e3ec20f784eb4f6d18513e953c
Don McCaughey <don@donm.cc>
2024-05-28 21:14:18 -0700

    "Un-inline" result functions.
    Make `result_is_success()`, `result_success()`, `result_system_error()`,
    `result_ncurses_err()`, `result_ncurses_errno()`,
    `result_ncurses_error()` and `result_set_system_error()` into plain
    functions.

--- src/base/result.c
+++ src/base/result.c
@@ -28,23 +28,60 @@ result_print_error(struct result result)
 }
 
 
-extern inline bool
-result_is_success(struct result result);
+bool
+result_is_success(struct result result)
+{
+    return result_type_success == result.type;
+}
 
-extern inline struct result
-result_success(void);
 
-extern inline struct result
-result_system_error(void);
+struct result
+result_success(void)
+{
+    return (struct result){ .type=result_type_success };
+}
 
-extern inline struct result
-result_ncurses_err(void);
 
-extern inline struct result
-result_ncurses_errno(void);
+struct result
+result_system_error(void)
+{
+    return (struct result){
+        .type=result_type_system_error,
+        .error_code=errno
+    };
+}
 
-extern inline struct result
-result_ncurses_error(int error_code);
 
-extern inline struct result
-result_set_system_error(int error_code);
+struct result
+result_ncurses_err(void)
+{
+    return (struct result){ .type=result_type_ncurses_err };
+}
+
+
+struct result
+result_ncurses_errno(void)
+{
+    return (struct result){
+        .type=result_type_ncurses_error,
+        .error_code=errno
+    };
+}
+
+
+struct result
+result_ncurses_error(int error_code)
+{
+    return (struct result){
+        .type=result_type_ncurses_error,
+        .error_code=error_code
+    };
+}
+
+
+struct result
+result_set_system_error(int error_code)
+{
+    errno = error_code;
+    return result_system_error();
+}
--- src/base/result.h
+++ src/base/result.h
@@ -13,67 +13,30 @@ struct result {
 };
 
 
-inline bool
-result_is_success(struct result result)
-{
-    return result_type_success == result.type;
-}
+bool
+result_is_success(struct result result);
 
+struct result
+result_success(void);
 
-inline struct result
-result_success(void)
-{
-    return (struct result){ .type=result_type_success };
-}
+struct result
+result_system_error(void);
 
     
-inline struct result
-result_system_error(void)
-{
-    return (struct result){
-        .type=result_type_system_error,
-        .error_code=errno
-    };
-}
+struct result
+result_ncurses_err(void);
 
-    
-inline struct result
-result_ncurses_err(void)
-{
-    return (struct result){ .type=result_type_ncurses_err };
-}
+struct result
+result_ncurses_errno(void);
 
-
-inline struct result
-result_ncurses_errno(void)
-{
-    return (struct result){
-        .type=result_type_ncurses_error,
-        .error_code=errno
-    };
-}
-
-
-inline struct result
-result_ncurses_error(int error_code)
-{
-    return (struct result){
-        .type=result_type_ncurses_error,
-        .error_code=error_code
-    };
-}
-
+struct result
+result_ncurses_error(int error_code);
 
 void
 result_print_error(struct result result);
 
-
-inline struct result
-result_set_system_error(int error_code)
-{
-    errno = error_code;
-    return result_system_error();
-}
+struct result
+result_set_system_error(int error_code);
 
 
 #endif
